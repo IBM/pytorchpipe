aef95efde9f0835c2f405166aec96ebe46d75034
tkornut <tkornut@us.ibm.com>
2019-03-29 11:07:23 -0700

    Loading pretrained embeddings model + wikitext sequence modeling task

--- ptp/components/models/__init__.py
+++ ptp/components/models/__init__.py
@@ -3,6 +3,7 @@ from .index_embeddings import IndexEmbeddings
 from .lenet5 import LeNet5
 from .model import Model
 from .rnn import RNN
+from .sentence_embeddings import SentenceEmbeddings
 from .softmax_classifier import SoftmaxClassifier
 
 __all__ = [
@@ -11,5 +12,6 @@ __all__ = [
     'LeNet5',
     'Model',
     'RNN',
+    'SentenceEmbeddings',
     'SoftmaxClassifier',
     ]
