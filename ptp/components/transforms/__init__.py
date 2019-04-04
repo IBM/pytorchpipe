da0c6a6931ae70e51f841e562558d720377606f9
tkornut <tkornut@us.ibm.com>
2019-04-04 16:47:42 -0700

    GlobalVariablePublisher, image size normalization in vqa_med problem, image_size classifier

--- ptp/components/transforms/__init__.py
+++ ptp/components/transforms/__init__.py
@@ -1,8 +1,10 @@
+from .global_variable_publisher import GlobalVariablePublisher
 from .list_to_tensor import ListToTensor
 from .reshape_tensor import ReshapeTensor
 
 
 __all__ = [
+    'GlobalVariablePublisher',
     'ListToTensor',
     'ReshapeTensor',
     ]
