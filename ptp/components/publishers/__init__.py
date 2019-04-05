a5fa2bf25134c3e33fdfdc8c8d655076ee1fb138
tkornut <tkornut@us.ibm.com>
2019-04-05 13:56:34 -0700

    reorganization: publishers

--- ptp/components/publishers/__init__.py
+++ ptp/components/publishers/__init__.py
@@ -1,7 +1,9 @@
 from .accuracy import Accuracy
 from .batch_size import BatchSize
+from .global_variable_publisher import GlobalVariablePublisher
 
 __all__ = [
     'Accuracy',
     'BatchSize',
+    'GlobalVariablePublisher',
     ]
--- ptp/components/transforms/__init__.py
+++ ptp/components/transforms/__init__.py
@@ -1,10 +1,8 @@
-from .global_variable_publisher import GlobalVariablePublisher
 from .list_to_tensor import ListToTensor
 from .reshape_tensor import ReshapeTensor
 
 
 __all__ = [
-    'GlobalVariablePublisher',
     'ListToTensor',
     'ReshapeTensor',
     ]
