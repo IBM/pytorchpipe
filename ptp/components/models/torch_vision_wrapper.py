130496f70781c0b3c28ca008daf33e68a64e5ca0
Tomasz Kornuta <43558388+tkornut@users.noreply.github.com>
2019-04-08 18:54:13 -0700

    Update torch_vision_wrapper.py
    author

--- ptp/components/models/torch_vision_wrapper.py
+++ ptp/components/models/torch_vision_wrapper.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__author__ = "Tomasz Kornuta & Vincent Marois"
+__author__ = "Tomasz Kornuta"
 
 
 import torch
