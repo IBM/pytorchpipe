c307e64f6a8d109bb3ac3075e2ae778a7fdc2ff4
tkornut <tkornut@us.ibm.com>
2019-04-10 11:09:04 -0700

    initial version of pipeline for testing VFLenet5, masks not working yet

--- ptp/components/models/__init__.py
+++ ptp/components/models/__init__.py
@@ -6,6 +6,7 @@ from .lenet5 import LeNet5
 from .model import Model
 from .recurrent_neural_network import RecurrentNeuralNetwork
 from .sentence_embeddings import SentenceEmbeddings
+from .variational_flow_lenet5 import VariationalFlowLeNet5
 
 __all__ = [
     'ConvNetEncoder',
@@ -16,4 +17,5 @@ __all__ = [
     'Model',
     'RecurrentNeuralNetwork',
     'SentenceEmbeddings',
+    'VariationalFlowLeNet5',
     ]
