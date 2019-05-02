e49c5566bca60d72cd4737ef76d186b4303143e1
tkornut <tkornut@us.ibm.com>
2019-05-02 16:22:45 -0700

    dataparallel: dummy dataset collate_fn

--- ptp/utils/app_state.py
+++ ptp/utils/app_state.py
@@ -155,7 +155,8 @@ class AppState(metaclass=SingletonMetaClass):
             print(type(datadict),"\n")
             data=datadict["index"]
             print("For: before to: input data ({}) size {}, device: {}\n".format(type(data), data.size(), data.device))
-            datadict = datadict.to(self.device)
+            # data = data.to(self.device)
+            datadict.to(self.device)
             print("For: before model: input data ({}) size {}, device: {}\n".format(type(data), data.size(), data.device))
             output = model(datadict)
             print("For: after model: output_size ", output.size(),"\n")
