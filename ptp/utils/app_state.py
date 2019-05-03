cd02b5f16efdbf34287bfe24098bf51c2614a41b
tkornut <tkornut@us.ibm.com>
2019-05-02 17:01:57 -0700

    dataparallel: dummy dataset self.data[index]

--- ptp/utils/app_state.py
+++ ptp/utils/app_state.py
@@ -156,11 +156,12 @@ class AppState(metaclass=SingletonMetaClass):
         for datadict in rand_loader:
             print(type(datadict),"\n")
             print(datadict)
+
             data=datadict["index"]
 
             print("For: before to: input data ({}) size {}, device: {}\n".format(type(data), data.size(), data.device))
-            # data = data.to(self.device)
-            datadict.to(self.device)
+            data = data.to(self.device)
+            #datadict.to(self.device)
             print("For: before model: input data ({}) size {}, device: {}\n".format(type(data), data.size(), data.device))
             output = model(datadict)
             print("For: after model: output_size ", output.size(),"\n")
