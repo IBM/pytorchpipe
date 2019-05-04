71425d2e6641a661496847ebf9d80368566ac15e
Tomasz Kornuta <tkornut@us.ibm.com>
2019-05-04 13:59:08 -0700

    Component: ReduceTensor + config, config c4_word_answer_glove_sum

--- ptp/components/transforms/__init__.py
+++ ptp/components/transforms/__init__.py
@@ -1,10 +1,12 @@
 from .concatenation import Concatenation
 from .list_to_tensor import ListToTensor
+from .reduce_tensor import ReduceTensor
 from .reshape_tensor import ReshapeTensor
 
 
 __all__ = [
     'Concatenation',
     'ListToTensor',
+    'ReduceTensor',
     'ReshapeTensor',
     ]
